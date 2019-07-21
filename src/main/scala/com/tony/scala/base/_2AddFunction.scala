d53da0ea40908ba5ea7f2cd3d7e4b433e962592c
tony <wabslygzj@163.com>
2019-07-21 11:00:28 +0800

    scala function for add

---
+++ src/main/scala/com/tony/scala/base/_2AddFunction.scala
@@ -0,0 +1,11 @@
+package com.tony.scala.base
+
+object _2AddFunction {
+  def main(args: Array[String]): Unit = {
+    println(add(1, 2))
+  }
+
+  def add(a: Int, b: Int): Int = {
+    a + b //return  a+b
+  }
+}
