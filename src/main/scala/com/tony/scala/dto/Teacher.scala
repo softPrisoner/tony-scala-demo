51c3bc58d2c1aeb00f3da8c1eaf9508a5841c528
tony <wabslygzj@163.com>
2019-07-21 12:39:55 +0800

    scala class and construct method with params

---
+++ src/main/scala/com/tony/scala/dto/Teacher.scala
@@ -0,0 +1,14 @@
+package com.tony.scala.dto
+
+class Teacher {
+  //声明_预留字段
+  var age: Int = _
+  var name: String = _
+
+  //构造函数
+  def this(age: Int, name: String) {
+    this()
+    this.age = age;
+    this.name = name;
+  }
+}
